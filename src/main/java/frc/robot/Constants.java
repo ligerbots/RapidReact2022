b80f0c2cf172d544d6372dc3f9751a29335c1e7a
junm.24 <junmoon06@gmail.com>
2022-01-17 15:45:55 -0500

    added constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,9 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final int ELEVATOR_LEADER_CAN_ID = 1;
+    public static final int ELEVATOR_FOLLOWER_CAN_ID = 2;
+    public static final int ARM_LEADER_CAN_ID = 3;
+    public static final int ARM_FOLLOWER_CAN_ID = 4;
+}
