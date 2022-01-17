bd767e38662c69b7c2a9f8fc682b563b243b5c3f
junm.24 <junmoon06@gmail.com>
2022-01-17 15:30:11 -0500

    added motor controllers and sensors

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -17,10 +17,10 @@ import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
 
-  CANSparkMax elevatorMotorLeader;
-  CANSparkMax elevatorMotorFollower;
-  TalonFX armMotorLeader;
-  TalonFX armMotorFollower;
+  CANSparkMax m_elevatorMotorLeader;
+  CANSparkMax m_elevatorMotorFollower;
+  TalonFX m_armMotorLeader;
+  TalonFX m_armMotorFollower;
   RelativeEncoder m_winchEncoder;
   DutyCycleEncoder m_armEncoder;
 
