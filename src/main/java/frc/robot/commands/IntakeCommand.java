a7527794f0650b41924b83cee984d327706e0c0b
junm.24 <junmoon06@gmail.com>
2022-01-21 18:23:24 -0500

    robotContainer

--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -1,8 +1,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Intake;
 
 public class IntakeCommand extends CommandBase {
   /**
