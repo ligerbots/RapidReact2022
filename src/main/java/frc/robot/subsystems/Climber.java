b613546d6698a1e12f5d73ecc224f933b555997b
Jack Wei <jack_weq@icloud.com>
2022-02-14 20:58:02 -0500

    added methods to update PID vals periodically

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -16,7 +16,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
-  double m_maxAccell, m_maxVel,m_allowedErr,m_minVel;
   CANSparkMax m_elevatorMotorLeader;
   CANSparkMax m_elevatorMotorFollower;
   TalonFX m_armMotorLeader;
@@ -25,6 +24,25 @@ public class Climber extends SubsystemBase {
   RelativeEncoder m_elevatorEncoder;
   SmartDashboard smartDashboard;
   SparkMaxPIDController m_elevatorPIDController, m_armPIDController;
+
+  // PID coefficients for Elevator
+  double m_kPElevator = 5e-5;
+  double m_kIElevator = 1e-6;
+  double m_kDElevator = 0; 
+  double m_kIzElevator = 0; 
+  double m_kFFElevator = 0.000156; 
+  double m_kMaxOutputElevator = 1; 
+  double m_kMinOutputElevator = -1;
+  
+  double m_elevatorMaxRPM = 5700;
+
+  // Smart Motion Coefficients for Elevator
+  double m_elevatorMaxVel = 2000; // rpm
+  double m_elevatorMinVel = 0;
+  double m_elevatorMaxAcc = 1500;
+
+  double m_elevatorAllowedErr = 0;
+
 
   public Climber() {
     m_elevatorMotorLeader = new CANSparkMax(Constants.ELEVATOR_LEADER_CAN_ID, MotorType.kBrushless);
@@ -35,37 +53,35 @@ public class Climber extends SubsystemBase {
     m_elevatorPIDController = m_elevatorMotorLeader.getPIDController();
     m_elevatorEncoder = m_elevatorMotorLeader.getEncoder();
 
-    m_elevatorPIDController.setP(Constants.kPElevator);
-    m_elevatorPIDController.setI(Constants.kIElevator);
-    m_elevatorPIDController.setD(Constants.kDElevator);
-    m_elevatorPIDController.setIZone(Constants.kIzElevator);
-    m_elevatorPIDController.setFF(Constants.kFFElevator);
-    m_elevatorPIDController.setOutputRange(Constants.kMinOutputElevator, Constants.kMaxOutputElevator);
+    m_elevatorPIDController.setP(m_kPElevator);
+    m_elevatorPIDController.setI(m_kIElevator);
+    m_elevatorPIDController.setD(m_kDElevator);
+    m_elevatorPIDController.setIZone(m_kIzElevator);
+    m_elevatorPIDController.setFF(m_kFFElevator);
+    m_elevatorPIDController.setOutputRange(m_kMinOutputElevator, m_kMaxOutputElevator);
 
     //set elevator PID smartMotion Coefficients
-    m_maxAccell = Constants.ElevatorMaxRPM;
-    m_maxVel = Constants.ElevatorMaxVel;
-
     int smartMotionSlot = 0;
-    m_elevatorPIDController.setSmartMotionMaxVelocity(m_maxVel, smartMotionSlot);
-    m_elevatorPIDController.setSmartMotionMinOutputVelocity(m_minVel, smartMotionSlot);
-    m_elevatorPIDController.setSmartMotionMaxAccel(m_maxAccell, smartMotionSlot);
-    m_elevatorPIDController.setSmartMotionAllowedClosedLoopError(m_allowedErr, smartMotionSlot);
+    m_elevatorPIDController.setSmartMotionMaxVelocity(m_elevatorMaxVel, smartMotionSlot);
+    m_elevatorPIDController.setSmartMotionMinOutputVelocity(m_elevatorMinVel, smartMotionSlot);
+    m_elevatorPIDController.setSmartMotionMaxAccel(m_elevatorMaxAcc, smartMotionSlot);
+    m_elevatorPIDController.setSmartMotionAllowedClosedLoopError(m_elevatorAllowedErr, smartMotionSlot);
     
     // display PID coefficients on SmartDashboard
-    SmartDashboard.putNumber("P Gain", Constants.kPElevator);
-    SmartDashboard.putNumber("I Gain", Constants.kIElevator);
-    SmartDashboard.putNumber("D Gain", Constants.kDElevator);
-    SmartDashboard.putNumber("I Zone", Constants.kIzElevator);
-    SmartDashboard.putNumber("Feed Forward", Constants.kFFElevator);
-    SmartDashboard.putNumber("Max Output", Constants.kMaxOutputElevator);
-    SmartDashboard.putNumber("Min Output", Constants.kMinOutputElevator);
+    SmartDashboard.putNumber("P Gain", m_kPElevator);
+    SmartDashboard.putNumber("I Gain", m_kIElevator);
+    SmartDashboard.putNumber("D Gain", m_kDElevator);
+    SmartDashboard.putNumber("I Zone", m_kIzElevator
+);
+    SmartDashboard.putNumber("Feed Forward", m_kFFElevator);
+    SmartDashboard.putNumber("Max Output", m_kMaxOutputElevator);
+    SmartDashboard.putNumber("Min Output", m_kMinOutputElevator);
 
     // display Smart Motion coefficients
-    SmartDashboard.putNumber("Max Velocity", m_maxVel);
-    SmartDashboard.putNumber("Min Velocity", m_minVel);
-    SmartDashboard.putNumber("Max Acceleration", m_maxAccell);
-    SmartDashboard.putNumber("Allowed Closed Loop Error", m_allowedErr);
+    SmartDashboard.putNumber("Max Velocity", m_elevatorMaxVel);
+    SmartDashboard.putNumber("Min Velocity", m_elevatorMinVel);
+    SmartDashboard.putNumber("Max Acceleration", m_elevatorMaxAcc);
+    SmartDashboard.putNumber("Allowed Closed Loop Error", m_elevatorAllowedErr);
     SmartDashboard.putNumber("Set Position", 0);
     SmartDashboard.putNumber("Set Velocity", 0);
 
@@ -74,6 +90,7 @@ public class Climber extends SubsystemBase {
   }
   public void periodic() {
     // This method will be called once per scheduler run
+    checkElevatorPIDVal();
   }
   
   // sets the elevator to a certain height
@@ -96,5 +113,54 @@ public class Climber extends SubsystemBase {
 
   }
 
+  private void checkElevatorPIDVal(){
+   // read PID coefficients from SmartDashboard
+   double p = SmartDashboard.getNumber("P Gain", 0);
+   double i = SmartDashboard.getNumber("I Gain", 0);
+   double d = SmartDashboard.getNumber("D Gain", 0);
+   double iz = SmartDashboard.getNumber("I Zone", 0);
+   double ff = SmartDashboard.getNumber("Feed Forward", 0);
+   double max = SmartDashboard.getNumber("Max Output", 0);
+   double min = SmartDashboard.getNumber("Min Output", 0);
+   double maxV = SmartDashboard.getNumber("Max Velocity", 0);
+   double minV = SmartDashboard.getNumber("Min Velocity", 0);
+   double maxA = SmartDashboard.getNumber("Max Acceleration", 0);
+   double allE = SmartDashboard.getNumber("Allowed Closed Loop Error", 0);
 
+   // if PID coefficients on SmartDashboard have changed, write new values to controller
+   if((p != m_kPElevator)) { m_elevatorPIDController.setP(p); m_kPElevator = p; }
+   if((i != m_kIElevator)) { m_elevatorPIDController.setI(i); m_kIElevator = i; }
+   if((d != m_kDElevator)) { m_elevatorPIDController.setD(d); m_kDElevator = d; }
+   if((iz != m_kIzElevator)) { m_elevatorPIDController.setIZone(iz); m_kIzElevator = iz; }
+   if((ff != m_kFFElevator)) { m_elevatorPIDController.setFF(ff); m_kFFElevator = ff; }
+   if((max != m_kMaxOutputElevator) || (min != m_kMinOutputElevator)) { 
+     m_elevatorPIDController.setOutputRange(min, max); 
+     m_kMinOutputElevator = min; m_kMaxOutputElevator = max; 
+   }
+   if((maxV != m_elevatorMaxVel)) { m_elevatorPIDController.setSmartMotionMaxVelocity(maxV,0); m_elevatorMaxVel = maxV; }
+   if((minV != m_elevatorMinVel)) { m_elevatorPIDController.setSmartMotionMinOutputVelocity(minV,0); m_elevatorMinVel = minV; }
+   if((maxA != m_elevatorMaxAcc)) { m_elevatorPIDController.setSmartMotionMaxAccel(maxA,0); m_elevatorMaxAcc = maxA; }
+   if((allE != m_elevatorAllowedErr)) { m_elevatorPIDController.setSmartMotionAllowedClosedLoopError(allE,0); m_elevatorAllowedErr = allE; }
+
+   double setPoint, processVariable;
+   boolean mode = SmartDashboard.getBoolean("Mode", false);
+   if(mode) {
+     setPoint = SmartDashboard.getNumber("Set Velocity", 0);
+     m_elevatorPIDController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
+     processVariable = m_elevatorEncoder.getVelocity();
+   } else {
+     setPoint = SmartDashboard.getNumber("Set Position", 0);
+     /**
+      * As with other PID modes, Smart Motion is set by calling the
+      * setReference method on an existing pid object and setting
+      * the control type to kSmartMotion
+      */
+     m_elevatorPIDController.setReference(setPoint, CANSparkMax.ControlType.kSmartMotion);
+     processVariable = m_elevatorEncoder.getPosition();
+   }
+   
+   SmartDashboard.putNumber("SetPoint", setPoint);
+   SmartDashboard.putNumber("Process Variable", processVariable);
+   SmartDashboard.putNumber("Output", m_elevatorMotorLeader.getAppliedOutput());
+ }
 }
