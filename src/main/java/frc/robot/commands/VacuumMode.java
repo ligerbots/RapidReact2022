79f635e139f8d835034da8544d0eadc5c37ea535
shadowdragon2805 <lexue28@gmail.com>
2022-03-13 21:56:53 -0400

    fixed vacuumMode

--- src/main/java/frc/robot/commands/VacuumMode.java
+++ src/main/java/frc/robot/commands/VacuumMode.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import frc.robot.Constants;
 import frc.robot.subsystems.Intake;
@@ -11,31 +10,21 @@ import frc.robot.subsystems.Shooter.ShooterSpeeds;
 public class VacuumMode extends CommandBase{
     Shooter m_shooter;
     Intake m_intake;
-    double m_distance;
-    ShooterSpeeds m_shooterSpeeds;
-
+    
     public VacuumMode(Shooter shooter, Intake intake){
         m_shooter = shooter;
         m_intake = intake;
 
-        SmartDashboard.putNumber("shooter/Shooter Top Speed", 0);
-        SmartDashboard.putNumber("shooter/Shooter Bottom Speed", 0);
-        SmartDashboard.putNumber("shooter/Chute Speed", 0);
     }
 
     @Override
     public void initialize() {
-        /*
-        double top = SmartDashboard.getNumber("shooter/Shooter Top Speed", 0);
-        double bottom = SmartDashboard.getNumber("shooter/Shooter Bottom Speed", 0);
-        m_shooter.setShooterRpms(top, bottom);*/
-
-        m_shooterSpeeds = Shooter.calculateShooterSpeeds(m_distance, false);//lowerhub speeds for shooter
-        m_shooter.setShooterRpms(m_shooterSpeeds.top, m_shooterSpeeds.bottom);
+        //turn on shooter for vacuum mode using lowHub values
+        ShooterSpeeds shooterSpeeds = Shooter.calculateShooterSpeeds(0.0, false);//lowerhub speeds for shooter
+        m_shooter.setShooterRpms(shooterSpeeds.top, shooterSpeeds.bottom);
 
         //turn on chute for vacuum mode
-        double chute = SmartDashboard.getNumber("shooter/Chute Speed", 0);
-        m_shooter.setChuteSpeed(chute);
+        m_shooter.setChuteSpeed(shooterSpeeds.chute);
 
         //turn on intake for vacuum mode
         m_intake.run(Constants.INTAKE_SHOOTING_SPEED);
