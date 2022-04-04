6e821c5caa307c1397d52da862ce8d64b4059b51
Charles Frankston <cbf@ligerbots.com>
2022-04-04 15:14:56 -0400

    Adjust constants back to Revere values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -96,8 +96,8 @@ public final class Constants {
     // We therefore subtract SHOOTER_POS from all shooting distance constants
     public static final double SHOOTER_POS = 24;
     
-    // Our usual "Tarmac" shot is 9ft
-    public static final double TARMAC_DEFAULT_DISTANCE = (12.0 * 9.0) - SHOOTER_POS;
+    // Our usual "Tarmac" shot 
+    public static final double TARMAC_DEFAULT_DISTANCE = 94.0;
     
     // distance from the edge of the field by the human shooting position
     public static final double JUST_OUTSIDE_TARMAC = (12.0 * 11.5) - SHOOTER_POS;
@@ -206,7 +206,8 @@ public final class Constants {
     public static final double CLIMB_TO_NEXT_BAR_TIMEOUT = 12.0;
 
     // robot starting distance
-    public static final double STARTING_DISTANCE = TARMAC_DEFAULT_DISTANCE - 10.0;
+    // this used to be TARMAC_DEFAULT_DISTANCE - 10.0, but adjusted to be closer at Revere
+    public static final double STARTING_DISTANCE = 74.0;
    
     // Xbox button mapping
     public static final int XBOX_A = 1;
