d0933b3aa81e4ce8fcfa700d9e6e0604586673fb
Jack Wei <jack_weq@icloud.com>
2022-04-14 14:45:23 -0400

    added extra button for resetting the elevator to limit switch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ClimbToNextBar;
+import frc.robot.commands.DescendElevatorToLimitSwitch;
 import frc.robot.commands.DriveCommand;
 import frc.robot.commands.ShooterCommand;
 import frc.robot.commands.TurnAndShoot;
@@ -119,6 +120,9 @@ public class RobotContainer {
         // JoystickButton farm4 = new JoystickButton(m_farm, 4);
         // farm4.whenPressed(new AdjustRobotAngleTest(m_driveTrain));
 
+        JoystickButton farm4 = new JoystickButton(m_farm, 4);
+        farm4.whenPressed(new DescendElevatorToLimitSwitch(m_climber));
+
         JoystickButton farm6 = new JoystickButton(m_farm, 6);
         farm6.whenPressed(new SetClimber(m_climber));
 
@@ -132,8 +136,8 @@ public class RobotContainer {
         farm11.whenPressed(new ResetClimber(m_climber));
 
         // Additional manual shooter position buttons(orange buttons)
-        JoystickButton farm4 = new JoystickButton(m_farm, 4);
-        farm4.whenPressed(new ShooterCommand(m_shooter, m_intake, Constants.TARMAC_DEFAULT_DISTANCE, true));
+        // JoystickButton farm4 = new JoystickButton(m_farm, 4);
+        // farm4.whenPressed(new ShooterCommand(m_shooter, m_intake, Constants.TARMAC_DEFAULT_DISTANCE, true));
     
         JoystickButton farm5 = new JoystickButton(m_farm, 5);
         farm5.whenPressed(new ShooterCommand(m_shooter, m_intake, Constants.JUST_OUTSIDE_TARMAC, true));
--- src/main/java/frc/robot/commands/DescendElevatorToLimitSwitch.java
+++ src/main/java/frc/robot/commands/DescendElevatorToLimitSwitch.java
@@ -5,7 +5,9 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.TrapezoidProfileCommand;
 import frc.robot.Constants;
 import frc.robot.subsystems.Climber;
@@ -13,62 +15,55 @@ import frc.robot.subsystems.Climber;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class DescendElevatorToLimitSwitch extends TrapezoidProfileCommand {
+public class DescendElevatorToLimitSwitch extends CommandBase {
   Climber m_climber;
+  double m_height;
 
-  // Did we hit the limit switch?
-  boolean[] m_hitLimitSwitch;
-  boolean[] m_limitSwitchAlreadyPressed;
+  double m_maxVel;
+  double m_maxAcc;
 
-  boolean[] m_ressetEncoder;
+  Command m_command;
 
   public DescendElevatorToLimitSwitch(Climber climber) {
-    super(
-        new TrapezoidProfile(
-            // Limit the max acceleration and velocity
-            new TrapezoidProfile.Constraints(
-                Constants.ELEVATOR_MAX_VEL_METER_PER_SEC_DESCEND_SLOW,
-                Constants.ELEVATOR_MAX_ACC_METER_PER_SEC_SQ_DESCEND_SLOW),
-            // End at desired position in meters; implicitly starts at 0
-            new TrapezoidProfile.State(Units.inchesToMeters(-30), 0),
-            // initial position state
-            new TrapezoidProfile.State(climber.getElevatorHeight()[0], 0)),
-        // Pipe the profile state to the drive
-        setpointState -> climber.setElevatorHeight(setpointState));
-        m_climber = climber;
+    this(climber, 0.0, Constants.ELEVATOR_MAX_VEL_METER_PER_SEC_DESCEND,
+        Constants.ELEVATOR_MAX_ACC_METER_PER_SEC_SQ_DESCEND);
   }
 
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {
-      m_hitLimitSwitch[0] = false;
-      m_hitLimitSwitch[1] = false;
-      m_ressetEncoder[0] = false;
-      m_ressetEncoder[1] = false;
-    }
+  public DescendElevatorToLimitSwitch(Climber climber, double height, final double MAX_VEL_METER_PER_SEC,
+      final double MAX_ACC_METER_PER_SEC) {
+    m_maxAcc = MAX_ACC_METER_PER_SEC;
+    m_maxVel = MAX_VEL_METER_PER_SEC;
+    m_climber = climber;
+    m_height = height;
+  }
 
-    @Override
-    public void execute() {
-        // We're going to need to check each elevator independently, so we need a loop
-        for (int i = 0; i <= 1; i++) {  
-          // Make sure that the switch is pressed twice in a row.
-          boolean tempPressedCheck = m_climber.m_limitSwitch[i].isPressed() &&
-              m_climber.getElevatorHeight()[i] < Constants.ELEVATOR_LIMIT_SWITCH_HEIGHT;
-          m_hitLimitSwitch[i] = tempPressedCheck && m_limitSwitchAlreadyPressed[i];
-          m_limitSwitchAlreadyPressed[i] = tempPressedCheck;
-  
-          // If we've hit the limit switch twice, we need to set the encoder value to limit switch height
-          // and then raise elevator to ELEVATOR_MIN_HEIGHT.
-          if (m_hitLimitSwitch[i] && m_limitSwitchAlreadyPressed[i] && !m_ressetEncoder[i]) {
-            m_climber.m_elevatorMotor[i].getEncoder().setPosition(Constants.ELEVATOR_LIMIT_SWITCH_HEIGHT);
-            m_climber.setOneElevatorHeight(i, new TrapezoidProfile.State(Constants.ELEVATOR_LIMIT_SWITCH_HEIGHT, 0.0));
-            m_ressetEncoder[i] = true;
-          }
-        }
+  @Override
+  public void initialize() {
+    m_height = Constants.HEIGHT_RESET_LIMIT_SWITCH;
+    m_command = new TrapezoidProfileCommand(new TrapezoidProfile(
+        // Limit the max acceleration and velocity
+        new TrapezoidProfile.Constraints(
+            m_maxVel,
+            m_maxAcc),
+        // End at desired position in meters; implicitly starts at 0
+        new TrapezoidProfile.State(m_height, 0),
+        // initial position state
+        new TrapezoidProfile.State(m_climber.getElevatorHeight()[0], 0)),
+        // Pipe the profile state to the drive
+        setpointState -> m_climber.setElevatorHeight(setpointState));
+
+    CommandScheduler.getInstance().schedule(m_command);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return m_command.isFinished();
   }
   
   @Override
-  public boolean isFinished() {
-    return m_hitLimitSwitch[0] && m_hitLimitSwitch[1];
+  public void end(boolean interrupted){
+    m_command.end(interrupted);
+    m_climber.m_elevatorMotor[0].getEncoder().setPosition(Constants.ELEVATOR_LIMIT_SWITCH_HEIGHT);
+    m_climber.m_elevatorMotor[1].getEncoder().setPosition(Constants.ELEVATOR_LIMIT_SWITCH_HEIGHT);
   }
 }
