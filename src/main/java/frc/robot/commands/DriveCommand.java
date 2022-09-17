15a6d5dcb67cdd3ba07e98ca3e4970df7f3e18bf
Jack Wei <jack_weq@icloud.com>
2022-09-17 09:57:18 -0400

    added sliding multiplier and dropdown tuning mode button

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -4,43 +4,58 @@
 
 package frc.robot.commands;
 
-
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
 
 public class DriveCommand extends CommandBase {
-  /** Creates a new DriveCommand. */
+	/** Creates a new DriveCommand. */
 
-  DriveTrain m_driveTrain;
-  DoubleSupplier m_throttle;
-  DoubleSupplier m_turn;
+	DriveTrain m_driveTrain;
+	DoubleSupplier m_throttle;
+	DoubleSupplier m_turn;
 
-  public DriveCommand(DriveTrain driveTrain, DoubleSupplier throttle, DoubleSupplier turn) {
-    m_driveTrain = driveTrain;
-    m_throttle = throttle;
-    m_turn = turn;
-    addRequirements(driveTrain);
-  }
+	double m_multiplier;
+	boolean m_isTuning;
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+	public DriveCommand(DriveTrain driveTrain, DoubleSupplier throttle, DoubleSupplier turn) {
+		m_driveTrain = driveTrain;
+		m_throttle = throttle;
+		m_turn = turn;
+		addRequirements(driveTrain);
+	}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		m_multiplier = 1;
+		m_isTuning = false;
+		SmartDashboard.putNumber("multiplier", m_multiplier);
+		SmartDashboard.putBoolean("isTuning", m_isTuning);
+	}
+
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
   public void execute() {
-    m_driveTrain.drive(m_throttle.getAsDouble(), m_turn.getAsDouble(), true);
+    m_isTuning = SmartDashboard.getBoolean("isTuning", false);
+    m_multiplier = SmartDashboard.getNumber("multiplier", 1);
+    
+    if(m_isTuning)
+        m_driveTrain.drive(m_throttle.getAsDouble()*m_multiplier, m_turn.getAsDouble()*m_multiplier, true);
+    else
+		m_driveTrain.drive(m_throttle.getAsDouble(), m_turn.getAsDouble(), true);
   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+	}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
 }
