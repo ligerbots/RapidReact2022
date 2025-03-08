1d3afa1430adb1caeb275cbf3514aab570459197
chenjayden <jaydenchen08@gmai.com>
2025-03-07 20:44:19 -0500

    Reduce apply config use

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -55,24 +55,27 @@ public class DriveTrain extends SubsystemBase {
 
     public DriveTrain() {
         // set factory default
-        m_leftMotor.getConfigurator().apply(new TalonFXConfiguration());
-        m_rightMotor.getConfigurator().apply(new TalonFXConfiguration());
-
-        // set configs
+        TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+        
         // set slot 0 gains
-        Slot0Configs slot0configs = new Slot0Configs();
+        Slot0Configs slot0configs = talonFXConfigs.Slot0;
+        
         slot0configs.kP = Constants.DRIVETRAIN_KP;
         slot0configs.kI = Constants.DRIVETRAIN_KI;
         slot0configs.kD = Constants.DRIVETRAIN_KD;
-
+        
         // m_leftMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
         m_leftMotor.setPosition(0);
-        
-        // slot0configs.(0, Constants.DRIVETRAIN_KF);
-        // m_leftLeader.setSensorPhase(false);
 
+        // apply configs
         m_leftMotor.getConfigurator().apply(slot0configs);
         m_rightMotor.getConfigurator().apply(slot0configs);
+
+        MotorOutputConfigs rightMotorConfigs = new MotorOutputConfigs();
+
+        // set invert to CW+ and apply config change
+        rightMotorConfigs.Inverted = InvertedValue.Clockwise_Positive;
+        m_leftMotor.getConfigurator().apply(rightMotorConfigs);
 
         // m_rightLeader.configFactoryDefault();
         // m_rightLeader.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
@@ -82,12 +85,6 @@ public class DriveTrain extends SubsystemBase {
         // m_rightLeader.config_kD(0, Constants.DRIVETRAIN_KD);
         // m_rightLeader.config_kF(0, Constants.DRIVETRAIN_KF);
         // m_rightLeader.setSensorPhase(true);
-        
-        MotorOutputConfigs rightMotorConfigs = new MotorOutputConfigs();
-
-        // set invert to CW+ and apply config change
-        rightMotorConfigs.Inverted = InvertedValue.Clockwise_Positive;
-        m_leftMotor.getConfigurator().apply(rightMotorConfigs);
 
         setMotorMode(NeutralModeValue.Coast);
         
